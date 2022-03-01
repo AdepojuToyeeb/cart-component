6593b122035e15e6bcb6fa4f5be3753866e29fc9
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-03-01 08:59:11 +0100

    removed hard-coded data and fetched data from an API

--- src/context.js
+++ src/context.js
@@ -1,12 +1,12 @@
 import React, {useContext, useEffect, useReducer} from "react";
 import reducer from './reducer';
-import cartItems from './data'
-
+// import cartItems from './data'
+const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
 const initialState ={
     loading: false,
-    cart: cartItems,
+    cart: [],
     total: 0,
     amount: 0
 }
@@ -26,6 +26,17 @@ const AppProvider = ({children}) =>{
     const decrease = (id) => {
         dispatch ({ type: 'DECREASE', payload: id})
     }
+
+    const fetchData = async()=>{
+        dispatch({type: 'LOADING'})
+        const response = await fetch(url)
+        const cart = await response.json()
+        dispatch({type:'DISPLAY_ITEMS', payload: cart})
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
 
     useEffect(()=>{
         dispatch ({ type: 'GET_TOTAL'})
